ebb3870677cf8b0bfc0664697e1a7fde72cf98f4
Mako <mnfeconicu41@gmail.com>
2019-08-21 11:46:03 +0900

    update disp.py

--- ___stash/02-structs-and-bytes/disp.py
+++ ___stash/02-structs-and-bytes/disp.py
@@ -5,41 +5,33 @@ def hexdump(data) -> str:
     return '\n'.join(__dumpgen(data))
 
 def __dumpgen(data):
-    '''
-    Generator that produces strings:
-    00000000: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  ................
-    '''
+    # Generator that produces strings (addr, hexstr, ascii):
+    # 00000000: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  ................
     generator = __chunks(data, 16)
     for addr, d in enumerate(generator):
-        # 00000000:
+        # address
         line = '%08X: ' % (addr*16)
-        # 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+        # hexstr
         dumpstr = __dump(d)
         line += dumpstr[:8*3]
-        if len(d) > 8:  # insert separator if needed
+        if len(d) > 8: # insert separator if needed
             line += ' ' + dumpstr[8*3:]
-        # ................
-        # calculate indentation, which may be different for the last line
+
+        # indent
         pad = 2
         if len(d) < 16:
             pad += 3 * (16 - len(d))
         if len(d) <= 8:
             pad += 1
         line += ' ' * pad
-
+        # ascii
         for byte in d:
             # printable ASCII range 0x20 to 0x7E
             line += chr(byte) if 0x20 <= byte <= 0x7E else '.'
         yield line
 
+# list(chunks([1,2,3,4,5,6,7], 3)) #=> [[1, 2, 3], [4, 5, 6], [7]]
 def __chunks(seq, size):
-    '''
-    Generator that cuts bytes into chunks of given size.
-    If `seq` length is not multiply of `size`, the lengh of the last chunk
-    returned will be less than requested.
-    >>> list( chunks([1,2,3,4,5,6,7], 3) )
-    [[1, 2, 3], [4, 5, 6], [7]]
-    '''
     d, m = divmod(len(seq), size)
     for i in range(d):
         yield seq[i*size:(i+1)*size]
