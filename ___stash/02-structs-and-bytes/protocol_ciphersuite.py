1ef6e90e4ac5ba962fd4edf545f2b4f9b7ab3a21
Mako <mnfeconicu41@gmail.com>
2019-08-25 08:32:36 +0900

    create CipherSuite.get_hash_{name,size}

--- ___stash/02-structs-and-bytes/protocol_ciphersuite.py
+++ ___stash/02-structs-and-bytes/protocol_ciphersuite.py
@@ -11,4 +11,18 @@ class CipherSuite(Enum):
     TLS_AES_128_CCM_8_SHA256     = Uint16(0x1305)
     TLS_EMPTY_RENEGOTIATION_INFO_SCSV = Uint16(0x00FF)
 
+    @classmethod
+    def get_hash_name(self, cipher_suite):
+        if cipher_suite == CipherSuite.TLS_AES_256_GCM_SHA384:
+            return 'sha384'
+        else:
+            return 'sha256'
+
+    @classmethod
+    def get_hash_size(self, cipher_suite):
+        if cipher_suite == CipherSuite.TLS_AES_256_GCM_SHA384:
+            return 48
+        else:
+            return 32
+
 CipherSuites = List(size_t=Uint16, elem_t=CipherSuite)
