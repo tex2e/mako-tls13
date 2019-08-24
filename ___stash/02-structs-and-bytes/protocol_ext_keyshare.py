9e13370a48dce01b71c33ff68adb80ada106e328
Mako <mnfeconicu41@gmail.com>
2019-08-24 15:26:19 +0900

    change KeyShare*Hello structure

--- ___stash/02-structs-and-bytes/protocol_ext_keyshare.py
+++ ___stash/02-structs-and-bytes/protocol_ext_keyshare.py
@@ -2,6 +2,7 @@
 from type import Uint16, Opaque, List
 import structmeta as meta
 
+from protocol_types import HandshakeType
 from protocol_ext_supportedgroups import NamedGroup
 
 OpaqueUint16 = Opaque(Uint16)
@@ -14,12 +15,11 @@ class KeyShareEntry(meta.StructMeta):
 KeyShareEntrys = List(size_t=Uint16, elem_t=KeyShareEntry)
 
 @meta.struct
-class KeyShareClientHello(meta.StructMeta):
-    client_shares: KeyShareEntrys
-
-@meta.struct
-class KeyShareServerHello(meta.StructMeta):
-    server_share: KeyShareEntry
+class KeyShareHello(meta.StructMeta):
+    shares: meta.Select('Handshake.msg_type', cases={
+        HandshakeType.client_hello: KeyShareEntrys,
+        HandshakeType.server_hello: KeyShareEntry,
+    })
 
 
 if __name__ == '__main__':
@@ -29,34 +29,48 @@ if __name__ == '__main__':
 
         def test_keyshare_clienthello(self):
 
-            ksch = KeyShareClientHello(
-                client_shares=KeyShareEntrys([
-                    KeyShareEntry(
-                        group=NamedGroup.x25519,
-                        key_exchange=OpaqueUint16(bytes.fromhex('01234567'))
-                    ),
-                    KeyShareEntry(
-                        group=NamedGroup.secp256r1,
-                        key_exchange=OpaqueUint16(bytes.fromhex('89abcdef'))
-                    )
-                ])
-            )
+            @meta.struct
+            class Handshake(meta.StructMeta):
+                msg_type: HandshakeType = HandshakeType.client_hello
+                fragment: KeyShareHello
 
-            ksch_byte = bytes.fromhex('0010 001d 0004 01234567 0017 0004 89abcdef')
-            self.assertEqual(bytes(ksch), ksch_byte)
-            self.assertEqual(KeyShareClientHello.from_bytes(bytes(ksch)), ksch)
+            handshake = Handshake(
+                fragment=KeyShareHello(
+                    shares=KeyShareEntrys([
+                        KeyShareEntry(
+                            group=NamedGroup.x25519,
+                            key_exchange=OpaqueUint16(bytes.fromhex('01234567'))
+                        ),
+                        KeyShareEntry(
+                            group=NamedGroup.secp256r1,
+                            key_exchange=OpaqueUint16(bytes.fromhex('89abcdef'))
+                        )
+                    ])
+                )
+            )
+            ksh = handshake.fragment
+            ksh_byte = bytes.fromhex('0010 001d 0004 01234567 0017 0004 89abcdef')
+            self.assertEqual(bytes(ksh), ksh_byte)
+            self.assertEqual(Handshake.from_bytes(bytes(handshake)), handshake)
 
         def test_keyshare_serverhello(self):
 
-            ksch = KeyShareServerHello(
-                server_share=KeyShareEntry(
-                    group=NamedGroup.x25519,
-                    key_exchange=OpaqueUint16(bytes.fromhex('01234567'))
+            @meta.struct
+            class Handshake(meta.StructMeta):
+                msg_type: HandshakeType = HandshakeType.server_hello
+                fragment: KeyShareHello
+
+            handshake = Handshake(
+                fragment=KeyShareHello(
+                    shares=KeyShareEntry(
+                        group=NamedGroup.x25519,
+                        key_exchange=OpaqueUint16(bytes.fromhex('01234567'))
+                    )
                 )
             )
-
-            ksch_byte = bytes.fromhex('001d 0004 01234567')
-            self.assertEqual(bytes(ksch), ksch_byte)
-            self.assertEqual(KeyShareServerHello.from_bytes(bytes(ksch)), ksch)
+            ksh = handshake.fragment
+            ksh_byte = bytes.fromhex('001d 0004 01234567')
+            self.assertEqual(bytes(ksh), ksh_byte)
+            self.assertEqual(Handshake.from_bytes(bytes(handshake)), handshake)
 
     unittest.main()
